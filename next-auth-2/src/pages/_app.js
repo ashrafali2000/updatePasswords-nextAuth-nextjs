dd45bce3ee6be7cb22042158b422d475aec3eb1a
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-08-29 14:57:49 +0500

    add SessionProvider in _appjs file

--- next-auth-2/src/pages/_app.js
+++ next-auth-2/src/pages/_app.js
@@ -1,9 +1,12 @@
 import '@/styles/globals.css'
-import Header from './components/header'
-
+import Header from '../components/header'
+import { SessionProvider } from 'next-auth/react'
 export default function App({ Component, pageProps }) {
   return <div>
+    <SessionProvider session={pageProps.session}> 
     <Header></Header>
     <Component {...pageProps} />
+
+    </SessionProvider>
     </div>
 }
