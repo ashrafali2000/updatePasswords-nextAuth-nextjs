f485a0cc6ccae80dd3cf48584035613cdbed80b5
Ashraf-ali-2000 <122337346+Ashraf-ali-2000@users.noreply.github.com>
2023-09-05 17:12:54 +0500

    update form file

--- next-auth-2/src/components/form.js
+++ next-auth-2/src/components/form.js
@@ -1,89 +1,171 @@
-import Link from "next/link"
-import { useRef } from "react"
+import Link from "next/link";
+import { useRef } from "react";
 
-export default function Form({signin,onFormSubmit, updatePassword}) {
+export default function Form({ signin, onFormSubmit, updatePassword }) {
   const emailRef = useRef();
   const passwordRef = useRef();
+  const oldPasswordRef = useRef();
+  const newPasswordRef = useRef();
+
+  const updatePasswordHandler = (event) => {
+    event.preventDefault();
+    const oldPassword = oldPasswordRef.current.value;
+    const newPassword = newPasswordRef.current.value;
+    //validations
+    onFormSubmit(oldPassword, newPassword);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    
     const email = emailRef.current.value;
     const password = passwordRef.current.value;
-
     //validations
     onFormSubmit(email, password);
   };
-    return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-           
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-             { signin ? "Sign in to your account" : "Sign up for new account"}
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    ref={emailRef}
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-              
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    ref={passwordRef}
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                 <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                {updatePassword ? "Update password" : signin ? " Sign in" : "Sign up"}
-                </button> 
-              </div>
-            </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              {signin ? <Link href="/auth/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                SignUp
-              </Link> :<Link href="/auth/signin" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                SiggIn
-              </Link> }
-            </p>
-          </div>
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            {updatePassword
+              ? "Update Your Password"
+              : signin
+              ? "Sign in to your account"
+              : "Sign up for new account"}
+          </h2>
         </div>
-      </>
-    )
-  }
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form
+            className="space-y-6"
+            onSubmit={updatePassword ? updatePasswordHandler : handleSubmit}
+          >
+            {updatePassword ? (
+              <div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Old Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      ref={oldPasswordRef}
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      New Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      ref={newPasswordRef}
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      ref={emailRef}
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      ref={passwordRef}
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+            )}
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                {updatePassword
+                  ? "Update password"
+                  : signin
+                  ? " Sign in"
+                  : "Sign up"}
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{" "}
+            {signin ? (
+              <Link
+                href="/auth/signup"
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                SignUp
+              </Link>
+            ) : (
+              <Link
+                href="/auth/signin"
+                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+              >
+                SiggIn
+              </Link>
+            )}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
