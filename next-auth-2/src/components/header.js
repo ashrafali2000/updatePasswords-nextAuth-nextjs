c37166b3144177efbb23af7b6cd2f0f386491d55
Ashraf-ali-2000 <122337346+Ashraf-ali-2000@users.noreply.github.com>
2023-09-06 09:59:29 +0500

    update signout file

--- next-auth-2/src/components/header.js
+++ next-auth-2/src/components/header.js
@@ -1,13 +1,29 @@
-import { signOut } from "next-auth/react"
-import Link from "next/link"
-export default function Header(){
-    return (
-<div>
-  <h1>Next Authentication</h1> <div>
-    <Link href={"/profile"}  className="shadow-md py-2 px-6 border-[1px] border-black rounded-full border-black font-semibold hover:bg-[#efefef]">Profile</Link>
-    <Link className="shadow-md py-2 px-6 border-[1px] text-white rounded-full font-semibold hover:bg-[#37f] bg-indigo-600" href={"/auth/signin"}>Signin</Link>
-    <button className="shadow-md py-2 px-6 border-[1px]  rounded-full font-semibold border-black " onClick={signOut}>Logout</button>
-  </div>
-</div>
-)
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+export default function Header() {
+  return (
+    <div>
+      <h1>Next Authentication</h1>{" "}
+      <div>
+        <Link
+          href={"/profile"}
+          className="shadow-md py-2 px-6 border-[1px] border-black rounded-full border-black font-semibold hover:bg-[#efefef]"
+        >
+          Profile
+        </Link>
+        <Link
+          className="shadow-md py-2 px-6 border-[1px] text-white rounded-full font-semibold hover:bg-[#37f] bg-indigo-600"
+          href={"/auth/signin"}
+        >
+          Signin
+        </Link>
+        <button
+          className="shadow-md py-2 px-6 border-[1px]  rounded-full font-semibold border-black "
+          onClick={signOut}
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
 }
